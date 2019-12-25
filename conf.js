3602377df324196fc3d6235c7010978e0239706b
Taylor Allen <28820818+allenbyus@users.noreply.github.com>
2019-12-25 15:33:12 +0800

    Update conf.js

--- conf.js
+++ conf.js
@@ -26,13 +26,13 @@ jsproxy_config({
         ['std.wac.workers.dev:443']:1
       }
     },
-     'c': {
+    'c': {
       hidden: false,
       label: 'C Node(Cambodia)',
       lines: {
         ['flora.syh.us:8443']:1
-      },
-     'd': {
+    },
+    'd': {
       hidden: false,
       label: 'D Node(Canada)',
       lines: {
